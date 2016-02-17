c0754bf929f2589bba501d561f99c61ad5e4e709
nalinc <nalin.chhibber@gmail.com>
2016-02-17 13:28:01 +0530

    update

--- app.js
+++ app.js
@@ -1,72 +1,129 @@
 var http = require("http");
 var server = http.createServer();
+var request = require("request");
+
 var fs = require( 'fs' );
 var path = require( 'path' );
 var url = require('url');
 var config = require('./config');
-var filePath = './data/';
+
 var args = process.argv.slice(2);
 
 
-var createDb =function(){
+var createdb =function(dbname){
 	//To do
-	console.log('creating database');
-	var options = {
-		method: 'GET', 
-		url:'https://nalinc.cloudant.com/dls-book-search/_all_docs',
-		headers: {'Authorization': 'Basic bmFsaW5jOmI0enoxbmc0'}
-	};
-	http.request(options,function(response){
-		console.log(response)
-		process.exit(0);
-	},function(err){
-		console.log(err);
-		process.exit(0);
-	});
+	console.log('creating database: '+dbname);
+	var options ={
+		'uri': config.cloudantUrl+dbname,
+		'method': 'PUT',
+		'timeout': 10000,
+		'followRedirect': true,
+		'maxRedirects': 10,
+		'headers': {'Authorization': 'Basic '+config.cloudantKey}
+	}
+	request(options, function(error, response, body) {
+	  console.log(body);
+	  console.log('Make sure you change the default database to:'+ dbname+' in "config/index.js"');
+	});	
 }
 
-var insertFile =function(){
+var insertFile =function(filepath){
 	//To do
-	console.log('insertins documents');
-	process.exit(0);
+	console.log('inserting document: '+filepath);
+	var obj = readFiles(filepath);
+	var options ={
+		'uri': config.cloudantUrl+config.cloudantDB,
+		'method': 'POST',
+		'timeout': 10000,
+		'json':true,
+		'body':obj,
+		'followRedirect': true,
+		'maxRedirects': 10,
+		'headers': {'Authorization': 'Basic '+config.cloudantKey}
+	}
+	request(options, function(error, response, body) {
+	  console.log(body);
+	});		
 }
 
 
-var insertFiles =function(){
+var insertFiles =function(filepath){
+	console.log('reading files from directory: '+filepath)
 	// Loop through all the files in the temp directory
-	fs.readdir( filePath, function( err, files ) {
+	fs.readdir( filepath, function( err, files ) {
 	        if( err ) {
-	        	console.log('check-1')
+	        	console.log('error occured while reading directory!')
 	            console.log(err);
 	        } 
 	        files.forEach( function( file, index ) {
-	                readFiles(file);
+	                var obj = readFiles(filepath+file);
+					var options ={
+						'uri': config.cloudantUrl+config.cloudantDB,
+						'method': 'POST',
+						'timeout': 10000,
+						'json':true,
+						'body':obj,
+						'followRedirect': true,
+						'maxRedirects': 10,
+						'headers': {'Authorization': 'Basic '+config.cloudantKey}
+					}
+					request(options, function(error, response, body) {
+					  console.log(file+' '+JSON.stringify(body));
+					});		               	
 	        } );
 	} );
-	process.exit(0);
 }
 
+var createDesignDocument= function(docName){
+	var designDoc= {
+		'_id': '_design/'+docName,
+		'indexes': {
+			'contentSearch':{
+				'analyzer':{
+					'name':config.defaultAnalyzer,
+					'default': config.defaultLanguage
+				},
+				'index': config.searchIndex
+			}
+		}
+	}
 
+	var options ={
+		'uri': config.cloudantUrl+config.cloudantDB+'_design/'+docName,
+		'method': 'PUT',
+		'timeout': 10000,
+		'json':true,
+		'body':designDoc,
+		'followRedirect': true,
+		'maxRedirects': 10,
+		'headers': {'Authorization': 'Basic '+config.cloudantKey}
+	}
+	request(options, function(error, response, body) {
+	  console.log(body);
+	});	
+}
 
-function readFiles(fileName){
-	var obj = JSON.parse(fs.readFileSync(filePath+fileName, 'utf8'));
-	console.log(obj.imgbook['page-number'])
+function readFiles(completefilepath){
+	return JSON.parse(fs.readFileSync(completefilepath, 'utf8'));
 }
 
 var availableCommands = {
-	'createDb': createDb,
-	'insertDoc': insertFile,
-	'bulkInsert': insertFiles
+	'createdb': createdb,
+	'insertdoc': insertFile,
+	'bulkinsert': insertFiles,
+	'designdoc': createDesignDocument
 } 
 
 function scriptUsage(){
 	console.log('\n\t\t\t--Script Usage--\n');
-	console.log('\t1) Create a database\n')	
-		console.log('\t\t`node app.js createDb <database_name>`\n');
+	console.log('\t1) Create a database(rarely needed)\n')	
+		console.log('\t\t`node app.js createdb <database_name>`\n');
 	console.log('\t2) Insert a single document\n')
-		console.log('\t\t`node app.js insertDoc <document_path>`\n');
+		console.log('\t\t`node app.js insertdoc <document_path>`\n');
 	console.log('\t3) Bulk insert multiple documents\n')
-		console.log('\t\t`node app.js bulkInsert <directory_path>`\n');	
+		console.log('\t\t`node app.js bulkinsert <directory_path>`\n');
+	console.log('\t4) Create a design document\n')
+		console.log('\t\t`node app.js designdoc <docname>`\n');			
 }
 
 
@@ -74,7 +131,7 @@ server.listen(8080);
 
 if(args[0] in availableCommands){
 	if(args[1]){
-		availableCommands[args[0]].call()
+		availableCommands[args[0]].call(this,args[1])
 	}else{
 		scriptUsage();
 		process.exit(0);
--- config/index.js
+++ config/index.js
@@ -1,7 +1,20 @@
 var config= {};
 
 config.cloudantUrl = "https://nalinc.cloudant.com/";
-config.cloudantDB = "dls-book-search";
+config.cloudantDB = "test/";
 config.cloudantKey = "bmFsaW5jOmI0enoxbmc0";
+
+config.defaultAnalyzer = "standard";
+config.defaultLanguage = "english";
+config.searchIndex = "function (doc) {\
+  index('content', doc.content['raw'],{'index':true,'store':true});\
+  index('product-name', doc.content['title'],{'index':false,'store':true});\
+  index('chapter-name', doc.imgbook['parent-breadcrumb'],{'index':false,'store':true});\
+  index('section-name', doc.imgbook['item-name'],{'index':false,'store':true});\
+  index('page-number', doc.imgbook['page-number'],{'index':false,'store':true});\
+  index('thumbnail', doc.imgbook['thumbnail'],{'index':false,'store':true});\
+  index('org-id', doc.context['org-id'],{'index':true, 'store':false});\
+  index('product-id', doc.context['product-id'],{'index':true, 'store':false});\
+}";
 
 module.exports = config;
